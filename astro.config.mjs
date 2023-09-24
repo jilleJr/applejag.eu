04e27d66eac2e5eec6cda2afa52e3b8a89ff548c
Kalle Fagerberg <kalle.f8+git@proton.me>
2023-09-24 21:25:09 +0200

    Formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://applejag.eu',
-	integrations: [mdx(), sitemap()],
+  site: 'https://applejag.eu',
+  integrations: [mdx(), sitemap()],
 });
